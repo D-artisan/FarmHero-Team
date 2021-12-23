f268c54c288cc19ec2c185d33aaf68223ba45844
D-artisan <dartisan.inc@gmail.com>
2021-12-23 15:35:46 +0000

    configs

--- backend/FarmHero/settings.py
+++ backend/FarmHero/settings.py
@@ -1,5 +1,7 @@
 import os
+from datetime import timedelta
 from pathlib import Path
+import urllib.parse
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.conf.global_settings import AUTH_USER_MODEL
@@ -16,6 +18,15 @@ SECRET_KEY = 'q3$pic)pt7%jt5g5pyby$x(((c+5f%!+($_$6-uca3_@+=1)nz'
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'farm-hero-app.herokuapp.com', '6a03-154-160-24-109.ngrok.io']
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://localhost:8000',
+    'http://localhost:9000',
+    'https://farm-hero-app.herokuapp.com'
+)
 
 # Application definition
 
@@ -136,6 +147,36 @@ from django.contrib.messages import constants as messages
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
+}
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # 'rest_framework_simplejwt.authentication.JWTTokenUserAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+      'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'ISSUER': None,'AUTH_HEADER_TYPES': ('Bearer', 'JWT'),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'JTI_CLAIM': 'jti',
 }
 
 # Email Config
